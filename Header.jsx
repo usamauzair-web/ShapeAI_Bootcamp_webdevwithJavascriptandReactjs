196701b1aa6f31f2ba5d8c1546de22630a9d75a7
usamauzair-web <79391584+usamauzair-web@users.noreply.github.com>
2021-06-27 17:59:42 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return(
+    <Header>
+      <h1>
+        SHAPE AI BOOTCAMPS
+      </h1>
+    </Header>
+  );
+}
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js </h1>
+      <p>A
+        A Beautiful Course Everyone Must Follow this Course. Thak you SHAPEAI FOR this wonderful Course
+      </p>
+    </div>
+  );
+}
+export default Info;
